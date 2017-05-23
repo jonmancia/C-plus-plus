eb4693a45840768dc2b34261d6f192b61f28ae6e
Jonathan Mancia <jonathanman2010@icloud.com>
2017-05-22 20:03:02 -0600

    Added lettergrade program

--- carExpensesCalc.cpp
+++ carExpensesCalc.cpp
@@ -1,8 +1,3 @@
-**********************************************************\
-* Author: Jonmancia
-* Title: Car Expenses Calc
-* Version: 1.0
-**********************************************************\
 #include <iostream>
 using namespace std;
 
